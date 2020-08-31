4117a9bae24253569b993c8f99a94cc1f6b8d942
Tristan <talaliberte@yahoo.com>
2020-08-31 03:08:31 -0500

    Auto stash before merge of "main" and "Project1A-SpaceShip/main"

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float _MoveSpeed = 12f;
+    [SerializeField] float _TurnSpeed = 3f;
+
+    Rigidbody _rb = null;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveShip();
+        TurnShip();
+    }
+
+    //ship momentum forwards and backwards
+    void MoveShip()
+    {
+        //S/down = -1, W/up = +1, none = 0. Scale by _MoveSpeed
+        float moveAmountThisFrame = Input.GetAxisRaw("Vertical") * -_MoveSpeed;
+        //directional inputs
+        Vector3 moveDirection = transform.forward * moveAmountThisFrame;
+        //apply movement to the object
+        _rb.AddForce(moveDirection);
+    }
+
+    //turning (key turning likely)
+    void TurnShip()
+    {
+        //A/left = -1, D/right = 1, none = 0
+        float turnAmountThisFrame = Input.GetAxisRaw("Horizontal") * _TurnSpeed;
+        //specify axis
+        Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
+        //spiin
+        _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+
+   
+}
